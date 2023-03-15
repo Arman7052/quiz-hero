0e371cf321775ee99759e95262e474c32614a11e
Arman7052 <99arman7052@gmail.com>
2023-03-15 21:49:11 +0600

    Add quizContainer id

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ startQuiz.addEventListener("click", () => {
 // All quiz data fetched from json
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
-  // fetching data problem solved //
+  // fetching data problem solved by adding () after res.json //
   const data = await res.json();
   quizData = data;
   console.log(quizData);
@@ -59,21 +59,23 @@ const displayQuiz = (data) => {
   if (!data) {
     quizContainer.innerHTML = "";
     return;
-  } 
-
-  data.forEach((quiz, i) => {
-    quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
-  <div class="flex items-center">
-    <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
-      ${i + 1}
+  } else {
+    data.forEach((quiz, i) => {
+      quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
+    <div class="flex items-center">
+      <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
+        ${i + 1}
+      </div>
+      <p class="text-gray-800 text-sm">${quiz.quetion}</p>
     </div>
-    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
-  </div>
-  <div class="grid grid-cols-2 gap-4 mt-5">
-    ${displayQuizOptions(quiz.options, i)}
-  </div>
-</div>`;
-  });
+    <div class="grid grid-cols-2 gap-4 mt-5">
+      ${displayQuizOptions(quiz.options, i)}
+    </div>
+  </div>`;
+    });
+  }
+
+  
 };
 
 // EventListener for quiz submit button
