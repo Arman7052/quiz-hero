faef6f91cac736edd91735d06a2d1872b30e5009
Arman7052 <99arman7052@gmail.com>
2023-03-15 22:26:25 +0600

    Submit Quiz button bug fixed

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const loadQuiz = async () => {
   // fetching data problem solved by adding () after res.json //
   const data = await res.json();
   quizData = data;
-  console.log(quizData);
+  // console.log(quizData);
   displayQuiz(data);
 };
 
@@ -66,6 +66,7 @@ const displayQuiz = (data) => {
       <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
         ${i + 1}
       </div>
+      
       <p class="text-gray-800 text-sm">${quiz.question}</p>
     </div>
     <div class="grid grid-cols-2 gap-4 mt-5">
@@ -79,7 +80,7 @@ const displayQuiz = (data) => {
 };
 
 // EventListener for quiz submit button
-document.querySelector("#submit").addEventlistener("click", () => {
+document.querySelector("#submit").addEventListener("click", () => {
   if (answers.length < 6) {
     return;
   }
